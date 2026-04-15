045e93ab7a8dd69a251b721b51918cbcf688251f
DevOps2025p <chakri.palaparthi@gmail.com>
2026-04-16 05:24:01 +0530

    first shell

---
+++ firstshell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Trump: Hi Mask How are you doing"
+echo "Mask: I'mdoing good Thanks for asking what about you"
+echo "Trump: I'm also fine Let me discuss new Projects"
+echo "Mask: Sure Trump, which Project you want to know"
+echo "Trump: Mask it is all about nuclear attacks"
+echo "Mask: where will you plan Trump"
