f76870ae22787e93277a8fe32ad1012f2eb68ddf
DevOps2025p <chakri.palaparthi@gmail.com>
2026-04-16 06:02:33 +0530

    third script

---
+++ thirdarg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2 How are you doing"
+echo "$PERSON2: I'mdoing good Thanks for asking what about you"
+echo "$PERSON1: I'm also fine Let me discuss new Projects"
+echo "$PERSON2: Sure $PERSON1, which Project you want to know"
+echo "$PERSON1: $PERSON2 it is all about nuclear attacks"
+echo "$PERSON2: where will you plan $PERSON1"
