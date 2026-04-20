97d421c5caa76c130dc68ccc30c291632af9065c
DevOps2025p <chakri.palaparthi@gmail.com>
2026-04-21 05:12:54 +0530

    special vars

--- specialvars.sh
+++ specialvars.sh
@@ -1,4 +1,10 @@
-!#/bin/bash
+#!/bin/bash
 echo "All the special variables in script here:: $@"
 echo "Script name is:: $0"
 echo "Number of arguments passed:: $#"
+echo "Current directory is:: $PWD"
+echo "User name is:: $USER"
+echo "Home directory is:: $HOME"
+echo "Current process id is:: $$"
+sleep 10 &
+echo "Background process id is:: $!"
