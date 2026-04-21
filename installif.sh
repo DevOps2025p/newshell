089a01f65d96d8246839d30a77cd61ea970d3e33
DevOps2025p <chakri.palaparthi@gmail.com>
2026-04-21 06:13:56 +0530

    special vars

--- installif.sh
+++ installif.sh
@@ -3,6 +3,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run the script as root user"
+    exit 1 #other then 0 all are error codes
 else
     echo "Script is running with root user"
 fi
