3391b166220689c1144ecd3e06e584872739fc11
DevOps2025p <chakri.palaparthi@gmail.com>
2026-04-26 15:41:33 +0530

    function

--- function.sh
+++ function.sh
@@ -6,22 +6,22 @@ LOGS_FOLDER="/var/log/shellscriptlogs"
 SCRIPT_NAME=$(echo $0 | cut -d'.' -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
-echo "Script execution started at: $(date)" &>> $LOG_FILE
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
 if [ $USERID -ne 0 ]
 then
-    echo "Please run the script as root user" &>> $LOG_FILE
+    echo "Please run the script as root user" | tee -a $LOG_FILE
     exit 1 #other then 0 all are error codes
 else
-    echo "Script is running with root user" &>> $LOG_FILE
+    echo "Script is running with root user" | tee -a $LOG_FILE
 fi
 
 
 VALIDATE() {
     if [ $1 -eq 0 ]
     then
-        echo "Installing $2 is successful" &>> $LOG_FILE
+        echo "Installing $2 is successful" | tee -a $LOG_FILE
     else
-        echo "Installing $2 failed" &>> $LOG_FILE
+        echo "Installing $2 failed" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -32,12 +32,12 @@ VALIDATE() {
 dnf list installed mysql &>> $LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "MySQL is not installed, installing now" &>> $LOG_FILE
-    dnf install mysql -y &>> $LOG_FILE
+    echo "MySQL is not installed, installing now" | tee -a $LOG_FILE
+    dnf install mysql -y | tee -a $LOG_FILE
     VALIDATE $? "MySQL"
 
 else
-    echo "MySQL is already installed" &>> $LOG_FILE
+    echo "MySQL is already installed" | tee -a $LOG_FILE
 fi
 
 ########Python3 installation
@@ -45,11 +45,11 @@ fi
 dnf list installed python3 &>> $LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "Python3 is not installed, installing now" &>> $LOG_FILE
-    dnf install python3 -y &>> $LOG_FILE
+    echo "Python3 is not installed, installing now" | tee -a $LOG_FILE
+    dnf install python3 -y | tee -a $LOG_FILE
     VALIDATE $? "Python3"   
     else
-    echo "Python3 is already installed" &>> $LOG_FILE
+    echo "Python3 is already installed" | tee -a $LOG_FILE
 fi
 
 #########################Nginx installation
@@ -57,9 +57,9 @@ fi
 dnf list installed nginx &>> $LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "Nginx is not installed, installing now" &>> $LOG_FILE
-    dnf install nginx -y &>> $LOG_FILE
+    echo "Nginx is not installed, installing now" | tee -a $LOG_FILE
+    dnf install nginx -y | tee -a $LOG_FILE
     VALIDATE $? "Nginx"
 else
-    echo "Nginx is already installed" &>> $LOG_FILE
+    echo "Nginx is already installed" | tee -a $LOG_FILE
 fi
